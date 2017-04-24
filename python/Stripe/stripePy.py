38baafd3f6f6c10749877492e30ad4c10dceaa51
Gwenole Roton <gwenole.roton@imerir.com>
2017-04-24 16:24:30 +0200

    Cors

--- python/Stripe/stripePy.py
+++ python/Stripe/stripePy.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import stripe
 import MySQLdb
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 db = MySQLdb.connect(host="docker-pi.local",
                      user="root",
