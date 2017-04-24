b4dfe325fba3c9e3ca82be180b9ea17ffc7408ac
Gwenole Roton <gwenole.roton@imerir.com>
2017-04-24 21:39:37 +0200

    Add payment.py

---
+++ python/payment.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template, make_response
+import stripe,json
+import MySQLdb
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+db = MySQLdb.connect(host="192.168.0.13",
+                     user="root",
+                     passwd="calendar_bdd",
+                     db="ical")
+
+cur = db.cursor()
+
+
+@app.route("/payment", metods=['POST'])
+def payment():
+    cur.execute("SELECT credit FROM user WHERE rfid=\'"+request.form['rfid']+"\'")
+
+    credit = cur[0] - 5
+
+    cur.execute("UPDATE user SET credit="+str(credit)+" WHERE rfid=\'"+request.form['rfid']+"\'")
+
+    resp = make_response("OK", 201)
+
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
