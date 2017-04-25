585b86152444d275555aa93ee6dac1cb40f58661
Flavian Amiel <flavian.amiel-nabot@imerir.com>
2017-04-25 10:28:01 +0200

    Code cleaning and comments.

--- node/ical_manager.js
+++ node/ical_manager.js
@@ -2,17 +2,17 @@ let app = require('express')();
 let server = require('http').Server(app);
 let mysql = require('mysql');
 let qs = require('querystring');
-let io = require('socket.io')(server)
+let io = require('socket.io')(server);
 
-let ical = require('ical')
+let ical = require('ical');
 
-let parameters = require('./params.json')
+let parameters = require('./params.json');
 let groups = require('./groups.json');
 
-
-console.log("Lancement du serveur...")
+//Lets launch the server!
+console.log("Lancement du serveur...");
 server.listen(80);
-console.log("Serveur ok.")
+console.log("Serveur ok.");
 
 
 //SQL connection
@@ -23,7 +23,7 @@ let sql_connection = mysql.createConnection({
     database: parameters.db.database
 });
 
-
+//Route by default. Also the page displayed to the client.
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
@@ -32,13 +32,15 @@ app.get('/', function (req, res) {
 //manage user creation
 app.post('/addUser', function (req, res) {
     body = '';
-    post = null
+    post = null;
     req.on('data', function (data) {
         body += data;
+        //if body is too fat, we'close the connection to avoid an overflow.
         if (body.length > 1e6)
             request.connection.destroy();
     });
 
+    //We'll wait the end signal of the request to treat it's content.
     req.on('end', function () {
         post = qs.parse(body);
         console.log(post)
@@ -53,8 +55,6 @@ app.post('/addUser', function (req, res) {
             res.send(500)
         }
     });
-
-
 });
 
 //returns
@@ -63,23 +63,25 @@ app.post('/promo', function (req, res) {
     post = null
     req.on('data', function (data) {
         body += data;
+        //same as above. Don't flood us !
         if (body.length > 1e6)
             request.connection.destroy();
     });
 
+    //same as above.
     req.on('end', function () {
         post = qs.parse(body);
         console.log(post);
-        let rfid = getPromo(post.rfid)
-        console.log('getPRomo sent :'+rfid)
+        let rfid = getPromo(post.rfid);
+        console.log('getPRomo sent :' + rfid);
         res.send(rfid)
     });
 });
 
-
+//Route to add some users.
 app.get('/addUser', function (req, res) {
     res.sendfile(__dirname + '/formulaire.html');
-})
+});
 
 
 //returns the promotion of a given rfid
@@ -95,7 +97,7 @@ function getPromo(rfid) {
                     response.promotion = results[0].promotion;
                     console.log('The solution is: ', response);
 
-                    getIcal(response)
+                    getIcal(response);
                     return 200;
                 } catch (e) {
                     console.log('Error : sending 404');
@@ -103,35 +105,24 @@ function getPromo(rfid) {
                 }
             }
         );
-
-
     } catch (e) {
-        console.log("Error SQL !")
+        console.log("Error SQL !");
         return 404
     }
 }
 
-
+//Triggers the render event, and send the new calendar to the client
 function getIcal(user) {
-    let today = new Date();
-    console.log(today);
-
     let icalDay = "<iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=http%3A%2F%2Fical.imerir.com%2F" + user.promotion + "&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=250></iframe>"
     let icalWeek = "<iframe id='cv_if5' src='http://cdn.instantcal.com/cvir.html?id=cv_nav5&file=http%3A%2F%2Fical.imerir.com%2F" + user.promotion + "&theme=BL&ccolor=%23ffffc0&dims=1&gtype=cv_daygrid&gcloseable=0&gnavigable=1&gperiod=day7&itype=cv_simpleevent' allowTransparency=true scrolling='no' frameborder=0 height=600 width=800></iframe>"
-
-    io.to('lobby').emit('render', icalDay, icalWeek)
-
+    io.to('lobby').emit('render', icalDay, icalWeek);
 }
 
-
+//websocket management. Just to check if a client is here, and assure that he's routed to the good room.
 io.on('connection', function (socket) {
-    socket.join('lobby')
+    socket.join('lobby');
     socket.on('coucou', function () {
-        console.log("Client connecté !")
+        console.log("Client connecté !");
     });
-
-    socket.on('rfid', function (rfid) {
-        getPromo(rfid)
-    })
 });
 
